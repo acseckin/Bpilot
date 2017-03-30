bee2c55908a91e0e319fe9d191526cd7829a8be1
A. Çağdaş SEÇKİN <cagdaseckin@gmail.com>
2017-03-30 13:09:49 +0300

    Create test_mw_xbee.py

---
+++ test_mw_xbee.py
@@ -0,0 +1,22 @@
+from pyMultiwii import MultiWii
+import acsxbee
+
+board = MultiWii("/dev/ttyO1")
+
+
+xb=acsxbee.xbee()
+
+altitude=[0,0,0,0]
+PID=[0,0,0,0,0,0,0,0,0]
+rc=[0,0,0,0]
+
+try:
+    while True:
+        board.getData(MultiWii.ATTITUDE)
+        altitude[0]=round(float(board.attitude['angx']),3)
+        altitude[1]=round(float(board.attitude['angy']),3)
+        altitude[2]=round(float(board.attitude['heading']),3)
+        altitude[3]=round(float(board.attitude['elapsed']),3)
+        xb.reportMW(altitude,PID,rc)
+except Exception,error:
+    print "Error on Main: "+str(error)
