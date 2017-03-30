8ddf6548f7641a14677929bfbfc97ef6c0885cf1
A. Çağdaş SEÇKİN <cagdaseckin@gmail.com>
2017-03-30 14:53:48 +0300

    Update hcsr04.py

--- hcsr04.py
+++ hcsr04.py
@@ -1,26 +1,26 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
-GPIO.setup("P8_8", GPIO.OUT)
-GPIO.setup("P8_10", GPIO.IN)
+GPIO.setup("P8_10", GPIO.OUT)
+GPIO.setup("P8_8", GPIO.IN)
 
 temperature=28.0
 speedSound=33100.0+0.6*temperature
 loop=0
 while (True):
-        GPIO.output("P8_8", GPIO.LOW)
+        GPIO.output("P8_10", GPIO.LOW)
         time.sleep(0.1)
-        GPIO.output("P8_8", GPIO.HIGH)
+        GPIO.output("P8_10", GPIO.HIGH)
         time.sleep(10/1000000.0)
-        GPIO.output("P8_8", GPIO.LOW)
+        GPIO.output("P8_10", GPIO.LOW)
 
         start=time.time()
         stop=0
         loop=time.time()
-        while (GPIO.input("P8_10")!=True):
+        while (GPIO.input("P8_8")!=True):
                 start=time.time()
                 if start-loop>1:
                         break
-        while(GPIO.input("P8_10")==True):
+        while(GPIO.input("P8_8")==True):
                 stop=time.time()
                 if stop-start>1:
                         break
