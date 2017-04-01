eea1f1293bfe87de5849cbf2aba2793ca4ca35bd
acseckin <cagdaseckin@gmail.com>
2017-04-01 12:07:43 +0300

    12:07:43_01,04,2017

--- Pxbee.py
+++ Pxbee.py
@@ -14,23 +14,25 @@ class xbee():
         self.uname="UART"+str(port[-1])
         self.uart=UART.setup(self.uname)
         self.status=0
-        self.dx=0
-        self.dy=0
-        self.dz=0
-        self.dh=0
-        self.xkp=0
-        self.xki=0
-        self.xkd=0
-        self.ykp=0
-        self.yki=0
-        self.ykd=0
-        self.zkp=0
-        self.zki=0
-        self.zkd=0
-        self.hkp=0
-        self.hki=0
-        self.hkd=0
-        self.th=0
+    
+        self.roll=0
+        self.pitch=0
+        self.yaw=0
+        self.height=0
+        self.pos=[0,0]
+        self.rcchannels=[0,0,0,0]
+        
+        self.rollPID=[0,0,0]
+        self.pitchPID=[0,0,0]
+        self.yawPID=[0,0,0]
+        self.heightPID=[0,0,0]
+        self.posPID=[0,0,0]
+        
+        self.PIDCONT="$C"
+        self.MWII="$M"
+        self.POSITION="$P"
+        self.RCCHANNEL="$R"
+        
         try:
             self.ser= serial.Serial(port = self.port, baudrate=self.baud)
             self.ser.close()
@@ -46,98 +48,79 @@ class xbee():
             return [1,xbeein]
         else:
             return [0]
-    def write(self,send):
+    
+    def send(self,send):
         try:
-                self.ser.write(str(send))
-                self.ser.flush()
-                return [1, send]
+            self.ser.write(str(send))
+            self.ser.flush()
+            return [1, send]
         except ValueError:
-                return [0, ValueError]
-    def sendMWii(self,attitude,height):
+            return [0, ValueError]
+            
+    def sendMWii(self,attitude):
         attitude[0]= int(attitude[0]*10)
         attitude[1]= int(attitude[1]*10)
         attitude[2]= int(attitude[2]*10)
         attitude[3]= int(attitude[3]*1000)
         att=str(attitude)
-        height=str(int(height))
-        outstr="$MW:"+att+":"+height+":\n"
+        outstr=self.MWII+":"+att+":\n"
         outstr=outstr.replace("[","")
         outstr=outstr.replace("]","")
         outstr=outstr.replace(",",":")
-        return self.write(outstr)
-
+        return self.send(outstr)
+    
     def sendPID(self,PID):
         PID=str(PID)
-        outstr="$RC:"+PID+":\n"
+        outstr=self.PIDCONT+":"+PID+":\n"
         outstr=outstr.replace("[","")
         outstr=outstr.replace("]","")
         outstr=outstr.replace(",",":")
-        return self.write(outstr)
+        return self.send(outstr)
         
-    
     def sendRC(self,rcChannels):
         rcChannels=str(rcChannels)
-        outstr="$RC:"+rcChannels+":\n"
+        outstr=self.RCCHANNEL+":"+rcChannels+":\n"
         outstr=outstr.replace("[","")
         outstr=outstr.replace("]","")
         outstr=outstr.replace(",",":")
-        return self.write(outstr)
+        return self.send(outstr)
     
-    def sendGPS(self,latitude,longitute):
+    def sendGPS(self,latitude,longitute,height):
         longitute=str(round(longitute,3))
         latitude=str(round(latitude,3))
-        outstr="$RC:"+longitute+":"+latitude+":\n"
+        height=str(int(height))
+        outstr=self.POSITION+":"+longitute+":"+latitude+":\n"
         outstr=outstr.replace("[","")
         outstr=outstr.replace("]","")
         outstr=outstr.replace(",",":")
-        return self.write(outstr)
+        return self.send(outstr)
     
-  
-   
     def readDesired(self):
         try:
             rv=self.read()
             if rv[0]==1:
                 xbeein=rv[1].split(":")
-                if xbeein[0]=="$I":
-                    self.x=float(xbeein[1])
-                    self.y=float(xbeein[2])
-                    self.z=float(xbeein[3])
-                    self.h=float(xbeein[4])
-                elif xbeein[0]=="$D":
-                    self.dx=float(xbeein[1])
-                    self.dy=float(xbeein[2])
-                    self.dz=float(xbeein[3])
-                    self.dh=float(xbeein[4])
-                    print ("Desired Angles:",self.dx,self.dy,self.dz)
-                    self.reportTAR()
-                elif xbeein[0]=="$CX":
-                    self.xkp=float(xbeein[1])
-                    self.xki=float(xbeein[2])
-                    self.xkd=float(xbeein[3])
-                    print ("X PID:",self.xkp,self.xki,self.xkd)
-                    self.reportPID()
-                elif xbeein[0]=="$CY":
-                    self.ykp=float(xbeein[1])
-                    self.yki=float(xbeein[2])
-                    self.ykd=float(xbeein[3])
-                    print ("Y PID:",self.ykp,self.yki,self.ykd)
-                    self.reportPID()
-                elif xbeein[0]=="$CZ":
-                    self.zkp=float(xbeein[1])
-                    self.zki=float(xbeein[2])
-                    self.zkd=float(xbeein[3])
-                    print ("Z PID:",self.zkp,self.zki,self.zkd)
-                    self.reportPID()
-                elif xbeein[0]=="$CH":
-                    self.hkp=float(xbeein[1])
-                    self.hki=float(xbeein[2])
-                    self.hkd=float(xbeein[3])
-                    print ("H PID:",self.hkp,self.hki,self.hkd)
-                elif xbeein[0]=="$TH":
-                    self.th=float(xbeein[1])
-                    print ("Throttle",self.th)
-                    self.reportPID()
+                if (xbeein[0]==self.PIDCONT) and (len(xbeein)>=10):
+                    self.rollPID[0]=int(xbeein[1])
+                    self.rollPID[1]=int(xbeein[2])
+                    self.rollPID[2]=int(xbeein[3])
+                    self.pitchPID[0]=int(xbeein[4])
+                    self.pitchPID[1]=int(xbeein[5])
+                    self.pitchPID[2]=int(xbeein[6])
+                    self.yawPID[0]=int(xbeein[7])
+                    self.yawPID[1]=int(xbeein[8])
+                    self.yawPID[2]=int(xbeein[9])
+                    return self.rollPID,self.pitchPID,self.yawPID
+                elif (xbeein[0]==self.RCCHANNEL) and (len(xbeein)>=4):
+                    self.rcchannels[0]=int(xbeein[1])
+                    self.rcchannels[1]=int(xbeein[2])
+                    self.rcchannels[2]=int(xbeein[3])
+                    return self.rcchannels
+                elif (xbeein[0]==self.POSITION) and (len(xbeein)>=4):
+                    self.pos[0]=int(xbeein[1])
+                    self.pos[1]=int(xbeein[2])
+                    self.height=int(xbeein[3])
+                    return self.rcchannels
                 else:
                     print (xbeein)
         except ValueError:
