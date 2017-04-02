f2626b998554ffe6c2f067c66ae12748728ed66c
acseckin <cagdaseckin@gmail.com>
2017-04-02 21:16:06 +0300

    02/04/2017-[21:16:06]

--- Pgps.py
+++ Pgps.py
@@ -43,6 +43,7 @@ class gps(threading.Thread):
         self.gpsserial.flushInput()
     def readGPGGA(self):
         gpsinput=self.gpsserial.readline()
+        print gpsinput
         if "$GPGGA" in gpsinput:
             gga=gpsinput.split(',')
             print gga
@@ -53,8 +54,7 @@ class gps(threading.Thread):
                 self.altitude=float(gga[9])
                 self.latitude=[float(gga[2][:2]),float(gga[2][2:4]),float(gga[2][4:])]
                 self.longitude=[float(gga[4][:2]),float(gga[4][2:4]),float(gga[4][4:])]
-            else:
-                print gpsinput
+                
         self.gpsserial.flushInput()
 
     def bearing(self, lo1, la1, lo2, la2):
