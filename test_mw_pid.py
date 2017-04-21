aebd91ef8dd5a7276de70d4ddf41bc5e76be98c8
acseckin <cagdaseckin@gmail.com>
2017-04-21 23:34:04 +0300

    21/04/2017-[23:34:04]

--- test_mw_pid.py
+++ test_mw_pid.py
@@ -9,15 +9,16 @@ Created on Fri Apr 21 23:16:15 2017
 from Pmwii import MultiWii
 import numpy as np
 import time
+vals=[0,0,0,0,0,0,0,0,0]
 mw = MultiWii("/dev/ttyO1")
 i=0
 while True:
+    if time.time()%4>2:
       pidval=mw.getPID()
       print pidval
-      if time.time()%4>2:
-          i=i+1
-          vals=np.ones(9)*i
-          mw.setPID(vals)
-          print ">>",vals
-          if i>20:
-              i=0
+      i=i+1
+      vals[:]=[x+i for x in vals]
+      mw.setPID(vals)
+      print ">>",vals
+      if i>20:
+          i=0
